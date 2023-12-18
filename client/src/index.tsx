b705c0bab41d8e48f416c43bed2b533d0b763bd3
jordantab <jordantab20@gmail.com>
2023-12-17 22:33:26 -0800

    Created browser router and 404 error page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
+import RibbonStart from './routes/RibbonStart';
+import Riddle from './routes/Riddle';
+import Gift from './routes/Gift';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        // TODO: make this path dynamic
+        path: '/ribbons/tswift',
+        element: <RibbonStart />,
+      },
+      {
+        // TODO: make this path dynamic
+        path: '/ribbons/tswift/1',
+        element: <Riddle />,
+      },
+      {
+        // TODO: make this path dynamic
+        path: '/ribbons/tswift/gift',
+        element: <Gift />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
