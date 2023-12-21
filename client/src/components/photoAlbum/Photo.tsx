9b1331ab159ebd3410c791bbe3f77d9ce8e5b71c
jordantab <jordantab20@gmail.com>
2023-12-21 15:44:16 -0500

    Put photos into cards

--- client/src/components/photoAlbum/Photo.tsx
+++ client/src/components/photoAlbum/Photo.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 type PhotoProps = {
   source: string;
@@ -6,7 +8,20 @@ type PhotoProps = {
 
 function Photo(props: PhotoProps) {
   const { source } = props;
-  return <h1>{source}</h1>;
+  return (
+    <Container>
+      <Card>
+        {/* <Card.Img variant="top" src={source} /> */}
+        <Card.Body>
+          <Card.Title>{source}</Card.Title>
+          <Card.Text>
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
 
 export default Photo;
--- client/src/components/photoAlbum/PhotoAlbum.tsx
+++ client/src/components/photoAlbum/PhotoAlbum.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
 import Photo from './Photo';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 type PhotoAlbumProps = {
   photos: string[];
@@ -11,9 +13,17 @@ function PhotoAlbum(props: PhotoAlbumProps) {
   const { photos, goToNextStage } = props;
   // TODO: update type of photos
   function displayPhotos(photos: any): JSX.Element {
-    return photos.map((photo: string, index: number) => {
-      return <Photo key={index} source={photo} />;
-    });
+    return (
+      <Container>
+        <Row xs={2} className="g-2">
+          {photos.map((photo: string, index: number) => (
+            <Col key={index}>
+              <Photo source={photo} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
   }
   return (
     <Container>
