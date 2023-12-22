0947af988fba92e778aa8ea831e5bf3eb915eb4d
jordantab <jordantab20@gmail.com>
2023-12-21 19:35:38 -0500

    Updated the Ribbon template modals to have conditional buttons, a description, and an image carousel

--- client/src/components/templates/TemplatesContainer.tsx
+++ client/src/components/templates/TemplatesContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Modal } from 'react-bootstrap';
+import { Button, Carousel, Container, Modal } from 'react-bootstrap';
 import Template from './Template';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -83,6 +83,26 @@ function TemplatesContainer() {
       gift: 'Gift section',
       currentStage: 0, //enum on the backend, 0 for photos, 1 for riddle, 2 for gift
     },
+    {
+      title: 'Custom',
+      photos: ['photo10', 'photo11', 'photo12'],
+      riddles: [
+        {
+          type: 1, // going to use enum in the backend, 0 for short answer and 1 for multiple choice
+          question: 'Who is your valentine?',
+          options: ['Taylor', 'Alex', 'Jordan', 'Kevin'],
+          answer: 'Taylor',
+        },
+        {
+          type: 1, // going to use enum in the backend, 0 for short answer and 1 for multiple choice
+          question: 'What is her last name?',
+          options: ['Swift', 'Daddabbo', 'Tab', 'James'],
+          answer: 'Swift',
+        },
+      ],
+      gift: 'Gift section',
+      currentStage: 0, //enum on the backend, 0 for photos, 1 for riddle, 2 for gift
+    },
   ];
 
   const [showModal, setShowModal] = useState(false);
@@ -97,37 +117,51 @@ function TemplatesContainer() {
 
   function displayTemplates(templates: RibbonList): JSX.Element {
     return (
-      <Container>
-        <Row xs={2} className="g-2">
-          {templates.map((template: Ribbon, index: number) => (
-            <Col key={index} onClick={() => handleTemplateClick(template)}>
-              <Template template={template} />
-            </Col>
-          ))}
-        </Row>
-        <Modal
-          show={showModal}
-          onHide={handleClose}
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-          backdrop="static"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>{selectedTemplate?.title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {/* Display template details here */}
-            {/* Example: <p>{selectedTemplate?.description}</p> */}
-          </Modal.Body>
-        </Modal>
-      </Container>
+      <Row xs={2} className="g-2">
+        {templates.map((template: Ribbon, index: number) => (
+          <Col key={index} onClick={() => handleTemplateClick(template)}>
+            <Template template={template} />
+          </Col>
+        ))}
+      </Row>
     );
   }
 
   return (
     <Container className="hero-container">
       {displayTemplates(templates)}
+      <Modal
+        show={showModal}
+        onHide={handleClose}
+        backdrop="static"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedTemplate?.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Carousel for images */}
+          <Carousel>
+            {selectedTemplate?.photos.map((photo, idx) => (
+              <Carousel.Item key={idx}>
+                <img
+                  className="d-block w-100"
+                  src={photo}
+                  alt={`Slide ${idx}`}
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+          <p>
+            This is a description of the template. Customize this text to
+            provide more information about the template.
+          </p>
+          <Button variant="primary">
+            {selectedTemplate?.title === 'Custom' ? 'Get Started' : 'Send'}
+          </Button>
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 }
